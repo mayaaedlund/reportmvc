4e919db1ea37f73ccd87cd08c775832f49645644
mayaaedlund <maya.edlund@hotmail.com>
2024-05-13 19:30:59 +0200

    Kollat igenom CardHand, ändrade numberCard från NumberDice

--- src/Card/CardHand.php
+++ src/Card/CardHand.php
@@ -4,16 +4,39 @@ namespace App\Card;
 
 use App\Card\Card;
 
+
+/**
+ * Represents a hand of cards.
+ */
 class CardHand
 {
+
+    /**
+     * The cards in the hand.
+     *
+     * @var Card[]
+     */
     private $hand = [];
 
+
+    /**
+     * Adds a card to the hand.
+     *
+     * @param Card $card The card to add.
+     *
+     * @return void
+     */
     public function add(Card $card): void
     {
         $this->hand[] = $card;
     }
 
-    // Metod för att lägga till alla kort i ordning i handen
+   
+    /**
+     * Adds all cards in order to the hand (52 cards).
+     *
+     * @return void
+     */
     public function addOrderedCards(): void
     {
         for ($i = 1; $i <= 52; $i++) {
@@ -22,11 +45,22 @@ class CardHand
         }
     }
 
-    public function getNumberDices(): int
+    /**
+     * Gets the number of cards in the hand.
+     *
+     * @return int The number of cards.
+     */
+    public function getNumberCards(): int
     {
         return count($this->hand);
     }
 
+
+    /**
+     * Gets the string representation of each card in the hand.
+     *
+     * @return array The string representations of the cards.
+     */
     public function getString(): array
     {
         $values = [];
@@ -36,6 +70,12 @@ class CardHand
         return $values;
     }
 
+
+    /**
+     * Calculates the total points of the hand based on card values.
+     *
+     * @return int The total points of the hand.
+     */
     public function calculatePoints(): int
     {
         $cardPoints = new CardPoints();
