f260faeacd25858625a82b3df3ef69f23ae61817
mayaaedlund <maya.edlund@hotmail.com>
2024-05-13 21:16:22 +0200

    Uppdaterat kommentarer

--- tests/Card/CardPointsTest.php
+++ tests/Card/CardPointsTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 class CardPointsTest extends TestCase
 {
     /**
-     * Test that card points can be retrieved.
+     * Test that card points can be found.
      */
     public function testGetCardPoints()
     {
