3bd87472c8ec002cd631a16adb325552f3907bec
mayaaedlund <maya.edlund@hotmail.com>
2024-05-16 16:44:28 +0200

    Routes till bibliotek klara

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -50,10 +50,94 @@ class LibraryController extends AbstractController
     }
 
 
+    #[Route('/library/delete', name: 'book_del', methods: ['GET'])]
+    public function libraryDelete(): Response
+    {
+        return $this->render('library/delete.html.twig');
+    }
+
+    #[Route('/library/delete', name: 'book_delete')]
+    public function deleteBook(
+        Request $request,
+        ManagerRegistry $doctrine
+    ): Response {
+        $bookId = $request->request->get('book_id');
+
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Library::class)->find($bookId);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for ID ' . $bookId
+            );
+        }
+
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_view_all');
+    }
+
+    #[Route('/library/change/{id}', name: 'book_change')]
+    public function changeBook(
+        Request $request,
+        ManagerRegistry $doctrine,
+        int $id
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $library = $entityManager->getRepository(Library::class)->find($id);
+
+        if (!$library) {
+            throw $this->createNotFoundException(
+                'No book found for id ' . $id
+            );
+        }
+
+        return $this->render('library/change.html.twig', [
+            'library' => $library
+        ]);
+    }
 
 
-
-
+    #[Route('/library/changed/{id}', name: 'update_Book', methods: ['POST'])]
+    public function updateBook(
+        Request $request,
+        ManagerRegistry $doctrine,
+        int $id
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $library = $entityManager->getRepository(Library::class)->find($id);
+    
+        if (!$library) {
+            throw $this->createNotFoundException(
+                'No book found for id ' . $id
+            );
+        }
+    
+        // Hämta data från begäran
+        $requestData = $request->request->all();
+    
+        // Uppdatera bokens attribut om de inte är tomma
+        if (!empty($requestData['book'])) {
+            $library->setBook($requestData['book']);
+        }
+        if (!empty($requestData['isbn'])) {
+            $library->setISBN($requestData['isbn']);
+        }
+        if (!empty($requestData['author'])) {
+            $library->setAuthor($requestData['author']);
+        }
+        if (!empty($requestData['image'])) {
+            $library->setImage($requestData['image']);
+        }
+    
+        // Spara ändringarna
+        $entityManager->flush();
+    
+        // Omdirigera till sidan för att visa information om boken efter ändringarna
+        return $this->redirectToRoute('library_view_all');
+    }
+    
 
     #[Route('/library/create', name: 'book_add', methods: ['GET'])]
     public function libraryAdd(): Response
@@ -78,7 +162,7 @@ class LibraryController extends AbstractController
         $entityManager->persist($library);
         $entityManager->flush();
 
-        return new Response('Saved new book with id ' . $library->getId());
+        return $this->redirectToRoute('library_view_all');
     }
 }
 
